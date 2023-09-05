41d3446814d7ffde396faa18b685c248bf839a3c
renansouz <renanss2005@gmail.com>
2023-09-05 11:17:56 -0300

    Renan the best developer

--- Word-SearchRN/App.js
+++ Word-SearchRN/App.js
@@ -1,123 +1,144 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 
-const generateEmptyGrid = (rows, cols) => {
-
-
-  // const letters = ["A", "B","C","D","E", "F","G", "H","I", "J","K", "L","M", "N", "O", "P","Q", "R","S", "T","U", "V","W", "X","Y", "Z"]
-
-
-  const gerarLetraAleatoria = () => {
-    var indiceLetra = Math.floor(Math.random() * 26);
-  
-    return String.fromCharCode(65 + indiceLetra);
-  }
-
-
-  const wordsToFind = ["REACT", "NATIVE", "JAVASCRIPT", "DEVELOPMENT"];
-
-
-
-
-  var contX = 0
-  var contY = 0
-
-  return Array.from({ length: rows }, () =>
-    Array.from({ length: cols }, () => ({
-
-      if(){},
-
-
-      letter: (gerarLetraAleatoria()),
-      selected: false,
-    })),
-  );
-};
-
-const App = () => {
-  const [grid, setGrid] = useState(generateEmptyGrid(10, 10));
-  const [selectedWord, setSelectedWord] = useState('');
-  
-  useEffect(() => {
-    // Função para gerar palavras e preencher o grid
-    const generateGrid = () => {
-
-  
-      const newGrid = generateEmptyGrid(10, 10); // Use a função generateEmptyGrid para criar um grid vazio.
-  
-
-  
-      return newGrid;
+class WordSearchGame extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gridLetras: this.gerarGridAleatorio(),
+      listaPalavras: ['CAT', 'RULES', 'DOGS', 'TIGER', 'STARS'],
+      palavrasEncontradas: [],
     };
-  
-    // Inicialize o estado do grid chamando a função de geração
-    setGrid(generateGrid());
-
-  }, []);
-
-const handleCellPress = (row, col) => {
-
-  const updatedGrid = [...grid]; //variável responsável por recarregar a grade em sua forma padrão
-
-  // verifica se a célula está ou não selecionada
-  if (updatedGrid[row][col].selected) {
-    updatedGrid[row][col].selected = false;
-  } else {
-    updatedGrid[row][col].selected = true;
-
-    
-    // Atualizar a palavra selecionada com a letra da célula
-    const letter = updatedGrid[row][col].letter;
-
-
-
-    if(updatedGrid[row][col].selected == true){
-      setSelectedWord(selectedWord + letter);
-    }else{
-      
-    }
   }
 
-  // Atualiza a grade puxando a const criada acima
-  setGrid(updatedGrid);
-};
+  gerarGridAleatorio = () => {
+    const letrasPossiveis = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const gridSize = 12; // Tamanho do grid
+    const grid = [];
 
+    for (let i = 0; i < gridSize; i++) {
+      const row = [];
+      for (let j = 0; j < gridSize; j++) {
+        const randomIndex = Math.floor(Math.random() * letrasPossiveis.length);
+        row.push(letrasPossiveis.charAt(randomIndex));
+      }
+      grid.push(row);
+    }
 
+    return grid;
+  };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.gridContainer}>
-        {grid.map((row, rowIndex) => (
-          <View key={rowIndex} style={styles.row}>
-            {row.map((cell, colIndex) => (
-              <TouchableOpacity
-                key={colIndex}
-                style={[
-                  styles.cell,
-                  cell.selected && styles.selectedCell,
-                ]}
-                onPress={() => handleCellPress(rowIndex, colIndex)}
-              >
-                <Text>{cell.letter}</Text>
-              </TouchableOpacity>
-            ))}
-          </View>
-        ))}
+  adicionarPalavraAoGrid = (palavra) => {
+    const { gridLetras } = this.state;
+    const gridSize = gridLetras.length;
+
+    // Escolhe aleatoriamente uma direção (horizontal ou vertical)
+    const isHorizontal = Math.random() < 0.5;
+
+    if (isHorizontal) {
+      const y = Math.floor(Math.random() * gridSize);
+      const x = Math.floor(Math.random() * (gridSize - palavra.length + 1));
+      for (let i = 0; i < palavra.length; i++) {
+        gridLetras[y][x + i] = palavra[i];
+      }
+    } else {
+      const x = Math.floor(Math.random() * gridSize);
+      const y = Math.floor(Math.random() * (gridSize - palavra.length + 1));
+      for (let i = 0; i < palavra.length; i++) {
+        gridLetras[y + i][x] = palavra[i];
+      }
+    }
+
+    this.setState({ gridLetras });
+  };
+
+  componentDidMount() {
+    this.state.listaPalavras.forEach((palavra) => {
+      this.adicionarPalavraAoGrid(palavra);
+    });
+  }
+
+  handleCellClick = (x, y) => {
+    const letra = this.state.gridLetras[y][x];
+    if (!letra) return; // Célula vazia
+
+    const palavraEncontrada = this.state.listaPalavras.find((palavra) =>
+      this.palavraContemCoordenada(palavra, x, y)
+    );
+
+    if (palavraEncontrada) {
+      this.setState(
+        (prevState) => ({
+          palavrasEncontradas: [...prevState.palavrasEncontradas, palavraEncontrada],
+        }),
+        () => {
+          if (this.state.palavrasEncontradas.length === this.state.listaPalavras.length) {
+            Alert.alert('Parabéns!', 'Você encontrou todas as palavras.');
+          }
+        }
+      );
+    }
+  };
+
+  palavraContemCoordenada = (palavra, x, y) => {
+    const dx = [0, 1, 1, 1, 0, -1, -1, -1];
+    const dy = [-1, -1, 0, 1, 1, 1, 0, -1];
+
+    for (let i = 0; i < palavra.length; i++) {
+      if (
+        x + i * dx.every((val) => val === dx[0]) >= 0 &&
+        x + i * dx.every((val) => val === dx[0]) < this.state.gridLetras[0].length &&
+        y + i * dy.every((val) => val === dy[0]) >= 0 &&
+        y + i * dy.every((val) => val === dy[0]) < this.state.gridLetras.length &&
+        palavra[i] === this.state.gridLetras[y + i * dy[0]][x + i * dx[0]]
+      ) {
+        continue;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Caça-Palavras</Text>
+        <View style={styles.grid}>
+          {this.state.gridLetras.map((row, rowIndex) => (
+            <View key={rowIndex} style={styles.row}>
+              {row.map((cell, columnIndex) => (
+                <TouchableOpacity
+                  key={columnIndex}
+                  style={[styles.cell, this.state.palavrasEncontradas.includes(cell) && styles.foundCell]}
+                  onPress={() => this.handleCellClick(columnIndex, rowIndex)}
+                >
+                  <Text>{cell}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          ))}
+        </View>
+        <View style={styles.wordList}>
+          <Text>Palavras a encontrar:</Text>
+          {this.state.listaPalavras.map((palavra, index) => (
+            <Text key={index} style={this.state.palavrasEncontradas.includes(palavra) && styles.foundWord}>
+              {palavra}
+            </Text>
+          ))}
+        </View>
       </View>
-      <Text>Selected Word: {selectedWord}</Text>
-      {}
-    </View>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#555'
+    justifyContent: 'center',
   },
-  gridContainer: {
+  grid: {
     flexDirection: 'column',
   },
   row: {
@@ -126,14 +147,20 @@ const styles = StyleSheet.create({
   cell: {
     width: 30,
     height: 30,
-    borderWidth: 1,
-    borderColor: '#FFF',
     justifyContent: 'center',
     alignItems: 'center',
+    borderWidth: 1,
   },
-  selectedCell: {
-    backgroundColor: 'yellow',
+  foundCell: {
+    backgroundColor: 'green',
+  },
+  wordList: {
+    marginTop: 20,
+  },
+  foundWord: {
+    textDecorationLine: 'line-through',
+    color: 'green',
   },
 });
 
-export default App;
+export default WordSearchGame;
