fcfa84513133776ea2bee77637c8ecb947775eec
MuriloBC2 <murilobarrosdocarmo@gmail.com>
2023-10-29 18:51:57 -0300

    criando a branch e testando seleção de palavras

--- Word-SearchRN/App.js
+++ Word-SearchRN/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
 
 class WordSearchGame extends Component {
   constructor(props) {
     super(props);
     this.state = {
       gridLetras: this.gerarGridAleatorio(),
-      listaPalavras: ['HISTORIA', 'CIENCIA', 'MATEMATICA', 'INGLES', 'PORTUGUES'],
+      listaPalavras: ["ááááááá", "óóóóóóó", "úúúúúúú", "ííííííí", "ééééééé"],
       palavrasEncontradas: [],
     };
   }
 
   gerarGridAleatorio = () => {
-    const gridSize = 12; // Tamanho do grid
+    const gridSizeX = 17; // Tamanho do grid horizontal
+    const gridSizeY = 12; // Tamanho do grid vertical
     const grid = [];
 
-    for (let i = 0; i < gridSize; i++) {
+    for (let i = 0; i < gridSizeX; i++) {
       const row = [];
-      for (let j = 0; j < gridSize; j++) {
-        row.push('');
+      for (let j = 0; j < gridSizeY; j++) {
+        row.push("");
       }
       grid.push(row);
     }
@@ -26,19 +27,37 @@ class WordSearchGame extends Component {
     return grid;
   };
 
+  preencherGridComLetrasAleatorias = () => {
+    const { gridLetras } = this.state;
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Letras possíveis
+
+    for (let i = 0; i < gridLetras.length; i++) {
+      for (let j = 0; j < gridLetras[i].length; j++) {
+        if (gridLetras[i][j] === "") {
+          // Preencher com letra aleatória
+          const randomIndex = Math.floor(Math.random() * alphabet.length);
+          gridLetras[i][j] = alphabet[randomIndex];
+        }
+      }
+    }
+
+    this.setState({ gridLetras });
+  };
+
   adicionarPalavraAoGrid = (palavra) => {
     const { gridLetras } = this.state;
     const gridSize = gridLetras.length;
 
-    const directions = ['horizontal', 'vertical'];
-    const selectedDirection = directions[Math.floor(Math.random() * directions.length)];
+    const directions = ["horizontal", "vertical"];
+    const selectedDirection =
+      directions[Math.floor(Math.random() * directions.length)];
 
-    if (selectedDirection === 'horizontal') {
+    if (selectedDirection === "horizontal") {
       let x, y;
       do {
         x = Math.floor(Math.random() * (gridSize - palavra.length + 1));
         y = Math.floor(Math.random() * gridSize);
-      } while (!this.ehPosicaoValida(gridLetras, palavra, x, y, 'horizontal'));
+      } while (!this.ehPosicaoValida(gridLetras, palavra, x, y, "horizontal"));
 
       for (let i = 0; i < palavra.length; i++) {
         gridLetras[y][x + i] = palavra[i];
@@ -48,7 +67,7 @@ class WordSearchGame extends Component {
       do {
         x = Math.floor(Math.random() * gridSize);
         y = Math.floor(Math.random() * (gridSize - palavra.length + 1));
-      } while (!this.ehPosicaoValida(gridLetras, palavra, x, y, 'vertical'));
+      } while (!this.ehPosicaoValida(gridLetras, palavra, x, y, "vertical"));
 
       for (let i = 0; i < palavra.length; i++) {
         gridLetras[y + i][x] = palavra[i];
@@ -61,15 +80,15 @@ class WordSearchGame extends Component {
   ehPosicaoValida = (grid, palavra, x, y, direcao) => {
     const gridSize = grid.length;
 
-    if (direcao === 'horizontal') {
+    if (direcao === "horizontal") {
       for (let i = 0; i < palavra.length; i++) {
-        if (grid[y][x + i] !== '' && grid[y][x + i] !== palavra[i]) {
+        if (grid[y][x + i] !== "" && grid[y][x + i] !== palavra[i]) {
           return false;
         }
       }
     } else {
       for (let i = 0; i < palavra.length; i++) {
-        if (grid[y + i][x] !== '' && grid[y + i][x] !== palavra[i]) {
+        if (grid[y + i][x] !== "" && grid[y + i][x] !== palavra[i]) {
           return false;
         }
       }
@@ -82,6 +101,8 @@ class WordSearchGame extends Component {
     this.state.listaPalavras.forEach((palavra) => {
       this.adicionarPalavraAoGrid(palavra);
     });
+
+    this.preencherGridComLetrasAleatorias();
   }
 
   handleCellClick = (x, y) => {
@@ -95,11 +116,17 @@ class WordSearchGame extends Component {
     if (palavraEncontrada) {
       this.setState(
         (prevState) => ({
-          palavrasEncontradas: [...prevState.palavrasEncontradas, palavraEncontrada],
+          palavrasEncontradas: [
+            ...prevState.palavrasEncontradas,
+            palavraEncontrada,
+          ],
         }),
         () => {
-          if (this.state.palavrasEncontradas.length === this.state.listaPalavras.length) {
-            Alert.alert('Parabéns!', 'Você encontrou todas as palavras.');
+          if (
+            this.state.palavrasEncontradas.length ===
+            this.state.listaPalavras.length
+          ) {
+            Alert.alert("Parabéns!", "Você encontrou todas as palavras.");
           }
         }
       );
@@ -113,9 +140,11 @@ class WordSearchGame extends Component {
     for (let i = 0; i < palavra.length; i++) {
       if (
         x + i * dx.every((val) => val === dx[0]) >= 0 &&
-        x + i * dx.every((val) => val === dx[0]) < this.state.gridLetras[0].length &&
+        x + i * dx.every((val) => val === dx[0]) <
+          this.state.gridLetras[0].length &&
         y + i * dy.every((val) => val === dy[0]) >= 0 &&
-        y + i * dy.every((val) => val === dy[0]) < this.state.gridLetras.length &&
+        y + i * dy.every((val) => val === dy[0]) <
+          this.state.gridLetras.length &&
         palavra[i] === this.state.gridLetras[y + i * dy[0]][x + i * dx[0]]
       ) {
         continue;
@@ -129,28 +158,23 @@ class WordSearchGame extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Caça-Palavras</Text>
         <View style={styles.grid}>
           {this.state.gridLetras.map((row, rowIndex) => (
             <View key={rowIndex} style={styles.row}>
               {row.map((cell, columnIndex) => (
                 <TouchableOpacity
                   key={columnIndex}
-                  style={[styles.cell, this.state.palavrasEncontradas.includes(cell) && styles.foundCell]}
+                  style={[
+                    styles.cell,
+                    this.state.palavrasEncontradas.includes(cell) &&
+                      styles.foundCell,
+                  ]}
                   onPress={() => this.handleCellClick(columnIndex, rowIndex)}
                 >
                   <Text>{cell}</Text>
                 </TouchableOpacity>
               ))}
             </View>
-          ))}
-        </View>
-        <View style={styles.wordList}>
-          <Text>Palavras a encontrar:</Text>
-          {this.state.listaPalavras.map((palavra, index) => (
-            <Text key={index} style={this.state.palavrasEncontradas.includes(palavra) && styles.foundWord}>
-              {palavra}
-            </Text>
           ))}
         </View>
       </View>
@@ -161,31 +185,29 @@ class WordSearchGame extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   grid: {
-    flexDirection: 'column',
+    flexDirection: "column",
   },
   row: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   cell: {
     width: 30,
     height: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderWidth: 1,
   },
   foundCell: {
-    backgroundColor: 'green',
+    backgroundColor: "green",
   },
-  wordList: {
-    marginTop: 20,
-  },
+
   foundWord: {
-    textDecorationLine: 'line-through',
-    color: 'green',
+    textDecorationLine: "line-through",
+    color: "green",
   },
 });
 
